52447707f689d8766141274742b3fc15dffc9b8b
ut463 <matthewcomer@comcast.net>
2024-10-10 16:16:54 -0500

    index js

---
+++ index.js
@@ -0,0 +1,74 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter project title.',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of project.',
+    },
+    {
+        type: 'input',
+        name: 'installations',
+        message: 'Describe how to install and use this project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage guide.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'license used?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How would a different user contribute to the project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How should you run a test in this project?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter github link.',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email.',
+    },
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const html = generateMarkdown(data)
+
+        writeToFile('README-GEN.md', html);
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const inquirer = require('inquirer');
+const index = require('./index');
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const inquirer = require('inquirer');
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === 'APACHE 2.0'){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === 'GPL 3.0') {
+    return`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === 'BSD 3') {
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  } else (license === 'None')
+}
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
